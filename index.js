90fa2e688d7ab145f745344a9327cb7b09d14f40
WendelSimoes <wendel.simoes.br@gmail.com>
2021-10-21 10:09:50 -0300

    Finishing flexbox-toggler just for educational purposes

---
+++ index.js
@@ -0,0 +1,91 @@
+function setRowReverse() {
+    document.getElementsByClassName("main-container")[0].style.flexDirection = "row-reverse";
+}
+
+function setRow() {
+    document.getElementsByClassName("main-container")[0].style.flexDirection = "row";
+}
+
+function setColumn() {
+    document.getElementsByClassName("main-container")[0].style.flexDirection = "column";
+}
+
+function setColumnReverse() {
+    document.getElementsByClassName("main-container")[0].style.flexDirection = "column-reverse";
+}
+
+function setNoWrap() {
+    document.getElementsByClassName("main-container")[0].style.flexWrap = "nowrap";
+}
+
+function setWrap() {
+    document.getElementsByClassName("main-container")[0].style.flexWrap = "wrap";
+}
+
+function setWrapReverse() {
+    document.getElementsByClassName("main-container")[0].style.flexWrap = "wrap-reverse";
+}
+
+function setStart() {
+    document.getElementsByClassName("main-container")[0].style.justifyContent = "flex-start";
+}
+
+function setEnd() {
+    document.getElementsByClassName("main-container")[0].style.justifyContent = "flex-end";
+}
+
+function setCenter() {
+    document.getElementsByClassName("main-container")[0].style.justifyContent = "center";
+}
+
+function setSpaceBetween() {
+    document.getElementsByClassName("main-container")[0].style.justifyContent = "space-between";
+}
+
+function setSpaceAround() {
+    document.getElementsByClassName("main-container")[0].style.justifyContent = "space-around";
+}
+
+function setItems() {
+    document.getElementsByClassName("main-container")[0].style.alignItems = "center";
+}
+
+function setAlignItemsStretch() {
+    document.getElementsByClassName("main-container")[0].style.alignItems = "stretch";
+}
+
+function setAlignItemsStart() {
+    document.getElementsByClassName("main-container")[0].style.alignItems = "flex-start";
+}
+
+function setAlignItemsEnd() {
+    document.getElementsByClassName("main-container")[0].style.alignItems = "flex-end";
+}
+
+function setAlignItemsBaseline() {
+    document.getElementsByClassName("main-container")[0].style.alignItems = "baseline";
+}
+
+function setAlignContentCenter() {
+    document.getElementsByClassName("main-container")[0].style.alignContent = "center";
+}
+
+function setAlignContentStretch() {
+    document.getElementsByClassName("main-container")[0].style.alignContent = "stretch";
+}
+
+function setAlignContentStart() {
+    document.getElementsByClassName("main-container")[0].style.alignContent = "flex-start";
+}
+
+function setAlignContentEnd() {
+    document.getElementsByClassName("main-container")[0].style.alignContent = "flex-end";
+}
+
+function setAlignContentBetween() {
+    document.getElementsByClassName("main-container")[0].style.alignContent = "space-between";
+}
+
+function setAlignContentAround() {
+    document.getElementsByClassName("main-container")[0].style.alignContent = "space-around";
+}
